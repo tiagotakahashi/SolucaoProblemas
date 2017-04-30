76ffde385f5133197f92f80401a087c1e047d06c
tiago.takahashi <tiagokioshi@gmail.com>
2017-04-30 17:44:40 -0300

    Refatoração:  -  Retirado comentário do código  - Utilização de Operator Overloading para simplificar a comparação pra ver quem ganhou

--- 02RockPaperScissors/RockPaperScissors.Application/Model/Player.cs
+++ 02RockPaperScissors/RockPaperScissors.Application/Model/Player.cs
@@ -18,15 +18,50 @@ namespace RockPaperScissors.Application.DTO
         public Player(string name, string move)
         {
             Name = name;
-            //Move moveAux;
-            //if (Move.TryParse(move, true, out moveAux))
-            //    Move = moveAux;
             char moveAux;
             if (char.TryParse(move, out moveAux))
             {
                 Move = (Move)moveAux;
                 Move = Enum.IsDefined(typeof(Move), Move) ? Move : Move.Invalid;
             }            
-        } 
+        }
+        
+        public static Player operator *(Player p1, Player p2)
+        {            
+            switch (p1.Move)
+            {
+                case Move.Rock:
+                    return p2.Move == Move.Scissors ? p1 : p2;
+                    break;
+                case Move.Paper:
+                    return p2.Move == Move.Rock ? p1 : p2;
+                    break;
+                case Move.Scissors:
+                    return p2.Move == Move.Paper ? p1 : p2;
+                    break;
+                default:
+                    return p1;
+                    break;
+            }
+        }
+
+        public static bool operator ==(Player p1, Player p2) {
+            return p1.Move == p2.Move;
+        }
+
+        public static bool operator !=(Player p1, Player p2)
+        {
+            return p1.Move != p2.Move;
+        }
+
+        public override bool Equals (object obj)
+        {
+            return this.Move == ((Player)obj).Move;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
--- 02RockPaperScissors/RockPaperScissors.Application/Services/Game.cs
+++ 02RockPaperScissors/RockPaperScissors.Application/Services/Game.cs
@@ -29,10 +29,11 @@ namespace RockPaperScissors.Application.Services
             var playerOne = _listPlayer[0];
             var playerTwo = _listPlayer[1];
 
-            return ((playerOne.Move.Equals(Move.Rock) && playerTwo.Move.Equals(Move.Scissors)) ||
-                    (playerOne.Move.Equals(Move.Scissors) && playerTwo.Move.Equals(Move.Paper)) ||
-                    (playerOne.Move.Equals(Move.Paper) && playerTwo.Move.Equals(Move.Rock)) ||
-                    (playerOne.Move.Equals(playerTwo.Move))) ? playerOne : playerTwo;
+
+            if (playerOne == playerTwo)
+                return playerOne;
+
+            return playerOne * playerTwo;
         }
     }
 }
